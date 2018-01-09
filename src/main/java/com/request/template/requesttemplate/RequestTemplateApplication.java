dcc9e1cca844ff4658992fc9863e41245d2430a8
andreabo <carito.9314@gmail.com>
2018-01-09 11:53:11 -0500

    Initial commit

---
+++ src/main/java/com/request/template/requesttemplate/RequestTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.request.template.requesttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestTemplateApplication.class, args);
+	}
+}
